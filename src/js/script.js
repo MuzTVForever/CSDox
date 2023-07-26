28a13c41f928311d36a9ece75cd36bbbb82bd690
MuzMods <51329699+MuzTVForever@users.noreply.github.com>
2023-07-26 16:50:05 +0300

    CSDox v.01

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var navLinks = document.getElementById("links");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "block";
+    }
+}
